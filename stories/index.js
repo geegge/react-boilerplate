ee2da8e015391805b470b8937c714af23e1e8059
geegge <email@suepergue.de>
2018-11-14 22:59:10 +0100

    adds storybook draft

---
+++ stories/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+// import { Button } from '@storybook/react/demo';
+
+import { Button } from '../src/components/shared/Button';
+
+storiesOf('Button', module)
+  .add('with name', () => (
+    <Button name="Button" />
+  ));
