16a12108c1eabde25c71ba78f40270149a2ad0cd
geegge <email@suepergue.de>
2018-11-19 23:10:56 +0100

    fixes storybook integration

--- stories/index.js
+++ stories/index.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
 // import { Button } from '@storybook/react/demo';
 
-import { Button } from '../src/components/shared/Button';
+import Button from '../src/components/shared/Button'
 
 storiesOf('Button', module)
   .add('with name', () => (
