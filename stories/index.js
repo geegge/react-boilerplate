2a1a74799d995652d42447792452c05c03038348
guenther <email@suepergue.de>
2018-12-05 23:04:37 +0100

    fixes component import for storybook

--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { Button } from '@storybook/react/demo';
+import Button from '../src/components/shared/Button';
 
-import { Button } from '../src/components/shared/Button';
-
-storiesOf('Button', module)
-  .add('with name', () => (
-    <Button name="Button" />
-  ));
+storiesOf('Button', module).add('with name', () => <Button name="Button" />);
