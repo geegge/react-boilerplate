44a1a092fed472aee20d34a5c09e29836bea3549
guenther <email@suepergue.de>
2019-06-22 17:17:59 +0200

    adds styled Button example

--- src/components/shared/Button/Button.jsx
+++ src/components/shared/Button/Button.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  color: ${props => props.color || 'palevioletred'};
+  border: 2px solid ${props => props.color || 'palevioletred'};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+
+  &:hover {
+    background-color: ${props => props.color || 'palevioletred'};
+    color: white;
+  }
+`;
 
 function Button(props) {
-  const { name } = props;
+  const { name, color } = props;
   return (
-    <button type="button" name="button">
+    <StyledButton type="button" name="button" color={color}>
       Hello, {name}!
-    </button>
+    </StyledButton>
   );
 }
 
 Button.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  color: PropTypes.string
 };
 
 export default Button;
