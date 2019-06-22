a9d17ecbfa7b1cc3e0ccbd16ffe41f51d4ca1f4b
guenther <email@suepergue.de>
2019-06-22 17:18:23 +0200

    adds styled button to storybook

--- stories/index.jsx
+++ stories/index.jsx
@@ -3,3 +3,6 @@ import { storiesOf } from '@storybook/react';
 import Button from '../src/components/shared/Button';
 
 storiesOf('Button', module).add('with name', () => <Button name="Button" />);
+storiesOf('Button', module).add('with name and color', () => (
+  <Button name="Button" color="mediumspringgreen" />
+));
